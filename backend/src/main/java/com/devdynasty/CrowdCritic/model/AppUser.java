9837915630cc0f0c55f567d47d4d770a82e3c8bd
konstadinos-agoris <konstadinosaa@gmail.com>
2023-04-18 01:53:39 +0300

    user validation annotations

--- backend/src/main/java/com/devdynasty/CrowdCritic/model/AppUser.java
+++ backend/src/main/java/com/devdynasty/CrowdCritic/model/AppUser.java
@@ -2,6 +2,7 @@ package com.devdynasty.CrowdCritic.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,16 +19,32 @@ public class AppUser {
     private Integer id;
 
 
+    @NotNull
+    @NotBlank
+    @Size(min=4,max= 30,message = "name should be between 4 and 30 characters")
     private String name;
 
+    @NotNull
+    @NotBlank
+    @Size(min=4,max=30,message = "surname should be between 4 and 30 characters")
     private String surname;
 
 
+    @NotNull
+    @NotBlank
+    @Size(min=4,message = "username should be between 4 and 30 characters")
     private String username;
 
+    @NotBlank
+    @NotNull
+    @Email(message = "not email type")
     private String email;
 
+    @NotBlank
+    @NotNull
+    @Digits(fraction = 0,integer = 10)
     private String phoneNumber;
+
 
     private Role role;
 
