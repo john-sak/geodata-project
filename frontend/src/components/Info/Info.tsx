6e8be1902d70d96c47d660d01147ea4b4a565feb
dberos <sdi1600269@di.uoa.gr>
2023-05-24 18:51:16 +0300

    changes after project1 critic

--- frontend/src/components/Info/Info.tsx
+++ frontend/src/components/Info/Info.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import InfoTitle from './InfoTitle'
 import InfoSubtitle from './InfoSubtitle'
 import InfoImage from './InfoImage'
+import InfoButton from './InfoButton'
 
 const Info = () => {
   return (
-    <div className='w-[100%] h-[500px] bg-sky-700 flex flex-col lg:flex-row lg:items-center'>
+    <div className='w-[100%] h-[650px] bg-sky-700 flex flex-col lg:flex-row lg:items-center lg:h-[500px]'>
         <div className='w-[100%] h-[50%] flex flex-col items-center gap-7
         lg:w-[50%] lg:order-2 lg:h-[100%]'>
             <InfoTitle/>
             <InfoSubtitle/>
+            <InfoButton/>
         </div>
         <InfoImage/>
     </div>
--- frontend/src/components/Info/InfoTitle.tsx
+++ frontend/src/components/Info/InfoTitle.tsx
@@ -4,7 +4,7 @@ const InfoTitle = () => {
   return (
     <div className='w-[100%] mt-6 flex justify-center lg:justify-end'>
         <h3 className='w-[90%] text-xl text-center lg:text-end lg:mr-10 lg:mt-6 lg:text-2xl 2xl:w-[45%]'>
-            Crowd Critic από τον πολίτη, για τον πολίτη
+            Επισκέπτες στο <br/> Crowd Critic
         </h3>
     </div>
   )
