f159335d6acdfecdacb96e380814898ecbfbc5d6
konstadinos-agoris <konstadinosaa@gmail.com>
2023-07-03 14:58:15 +0300

    prefecture and keyword post,exceptionhandling

--- backend/src/main/java/com/devdynasty/CrowdCritic/service/PrefectureService.java
+++ backend/src/main/java/com/devdynasty/CrowdCritic/service/PrefectureService.java
@@ -1,6 +1,7 @@
 package com.devdynasty.CrowdCritic.service;
 
 
+import com.devdynasty.CrowdCritic.exception.PrefectureNotFoundException;
 import com.devdynasty.CrowdCritic.model.Prefecture;
 import com.devdynasty.CrowdCritic.repository.PrefectureRepository;
 import org.springframework.stereotype.Service;
@@ -24,25 +25,35 @@ public class PrefectureService {
                 .findAll();
     }
 
-    public Prefecture findPrefectureById(Integer id){
+    public Prefecture findPrefectureById(Integer id) throws PrefectureNotFoundException {
 
         return prefectureRepository
                 .findById(id)
-                .get();
+                .orElseThrow(() -> new PrefectureNotFoundException("Prefecture with  id "+id+" not found"));
 
     }
 
 
-    public Prefecture findPrefectureByName(String name){
+    public Prefecture findPrefectureByName(String name) throws PrefectureNotFoundException {
 
         return prefectureRepository
                 .findPrefectureByName(name)
-                .get();
+                .orElseThrow(() -> new PrefectureNotFoundException("Prefecture with  name "+name+" not found"));
 
 
     }
 
 
+    public Prefecture savePrefecture(Prefecture prefecture) {
+
+
+        return prefectureRepository.
+                findPrefectureByName(prefecture.getName())
+                .orElseGet(() -> prefectureRepository
+                .save(prefecture));
+
+
+    }
 
 }
 
