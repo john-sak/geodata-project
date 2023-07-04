f9d0d985965e78334479f5f55643fa08bb4b42b1
dberos <sdi1600269@di.uoa.gr>
2023-07-04 13:48:17 +0300

    handling zoom level for startcount total

--- frontend/src/components/Search/SearchMap.tsx
+++ frontend/src/components/Search/SearchMap.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'leaflet/dist/leaflet.css'
-import { MapContainer, TileLayer, Marker, Popup, Circle } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, Circle, useMapEvents } from 'react-leaflet'
 import { icon } from 'leaflet'
 import { ISearchRadius } from './ISearch'
 
@@ -13,10 +13,23 @@ const SearchMap = (props: ISearchRadius) => {
         iconSize: [32, 32]
     })
 
+    const handleZoomEnd = (e: any) => {
+        const zoomLevel = e.target.getZoom();
+        console.log('Zoom:', zoomLevel);
+    };
+
+    const MapEvents = () => {
+    useMapEvents({
+        zoomend: handleZoomEnd,
+    });
+    return null;
+    };
+
   return (
-    <MapContainer center={[37.98856735, 23.72533417]} zoom={13} scrollWheelZoom={false}
+    <MapContainer center={[37.98856735, 23.72533417]} zoom={13} maxZoom={15} scrollWheelZoom={false}
     className='h-[100%] w-[100%] z-0'
     >
+        <MapEvents/>
         <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
