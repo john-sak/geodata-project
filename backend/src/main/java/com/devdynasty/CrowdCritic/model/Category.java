ad3f1edefbfbf7ff11290c657f41b944aba3d4d7
john-sak <giannis.sak@icloud.com>
2023-07-05 18:17:59 +0300

    working on import, error with ids

--- backend/src/main/java/com/devdynasty/CrowdCritic/model/Category.java
+++ backend/src/main/java/com/devdynasty/CrowdCritic/model/Category.java
@@ -1,6 +1,8 @@
 package com.devdynasty.CrowdCritic.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
