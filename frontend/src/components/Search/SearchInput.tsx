6a3024a3d4d185fc11a40999474abfa690125f54
dberos <sdi1600269@di.uoa.gr>
2023-07-07 00:45:31 +0300

    continue search

--- frontend/src/components/Search/SearchInput.tsx
+++ frontend/src/components/Search/SearchInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React, { useState, useRef, useEffect, ChangeEvent, MouseEventHandler } from 'react'
 import { ISearchInputProps } from './ISearch'
 import useCloseModal from '@/hooks/useCloseModal';
 import SearchIcon from '@mui/icons-material/Search';
@@ -17,7 +17,25 @@ const SearchInput = (props: ISearchInputProps) => {
     const [clicked, setClicked] = useState(false);
 
     let ref = useCloseModal(() => {
-        setClicked(false);
+        // setClicked(false);
+
+        if(value.length !== 0) {
+            const substrings = value.split(',');
+            const validSubstrings = substrings.filter(substring => {
+                const trimmedSubstring = substring.trim();
+                return dummyData.some(option => option.name.toLowerCase() === trimmedSubstring.toLowerCase());
+            });
+            const updatedValue = validSubstrings.join(',');
+            if(updatedValue.length !== 0) {
+                handleInputChange(name, updatedValue + ',');
+            }
+            else {
+                handleInputChange(name, '');
+            }
+        }
+        
+
+        setFilteredData([]);
     })
 
     const inputRef = useRef<HTMLInputElement | null>(null);
@@ -50,6 +68,45 @@ const SearchInput = (props: ISearchInputProps) => {
         },
     ];
 
+    const [filteredData, setFilteredData] = useState<any>([]);
+
+    // const [userInput, setUserInput] = useState('');
+    const [selectedOptions, setSelectedOptions] = useState<string[]>([]);
+
+    const handleFilter = (e: ChangeEvent<HTMLInputElement>) => {
+        const parts = e.target.value.split(',');
+        const inputValue = parts[parts.length - 1].trim();
+        const filteredInput = dummyData.filter((value) => {
+            return value.name.toLowerCase().includes(inputValue.toLowerCase());
+        })
+        const isSpaceOrSequenceOfSpaces = /^\s*$/.test(inputValue);
+        if(isSpaceOrSequenceOfSpaces) {
+            setFilteredData([]);
+        }
+        else {
+            const inputAscending = [...filteredInput].sort((a, b) => {
+                return a.name > b.name ? 1 : -1
+            });
+            setFilteredData(inputAscending.slice(0, 4));
+        }
+        handleInputChange(name, e.target.value);
+        // setUserInput(e.target.value);
+    }
+
+    const handleSelect = (str: string) => {
+        const isAlreadySelected = selectedOptions.some(selectedOption => selectedOption === str);
+        if(!isAlreadySelected) {
+            // const optionName = str + ',';
+            // setUserInput(prevValue => prevValue + optionName);
+            setSelectedOptions(prevOptions => [...prevOptions, str]);
+        }
+    }
+
+    useEffect(() => { 
+        selectedOptions.length !==0 && handleInputChange(name, selectedOptions.join(',') + ',');
+        setFilteredData([]);
+    }, [selectedOptions])
+
   return (
     <div className='relative w-[100%] h-[17%] bg-sky-800'>
         <div className='w-[100%] mt-4 flex items-center justify-start'>
@@ -66,7 +123,7 @@ const SearchInput = (props: ISearchInputProps) => {
                 className='w-[85%] rounded-l-md outline-none'
                 type="text"
                 value={value}
-                onChange={(e) => handleInputChange(name, e.target.value)}
+                onChange={handleFilter}
                 ref={inputRef}
                 onFocus={handleInputFocus}
                 onBlur={handleInputBlur}
@@ -81,13 +138,14 @@ const SearchInput = (props: ISearchInputProps) => {
                 </div>
             </div>
             {
-                clicked &&
+                filteredData.length !==0 &&
                     <div className='absolute min-h-0 max-h-[160px] w-[92%] bg-white ml-4 z-10 rounded-b-md lg:w-[88%] xl:w-[90%]'>
                         {
                             dummyData.map((value) => {
                                 return(
                                     <p
                                     key={value.id}
+                                    onClick={() => handleSelect(value.name)}
                                     className='h-10 flex items-center justify-start pl-2 text-base hover:bg-sky-100 hover:cursor-pointer last:rounded-b-md'
                                     >
                                         {value.name}
