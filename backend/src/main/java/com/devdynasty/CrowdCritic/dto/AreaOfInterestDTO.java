2306ea8fe77f4836b1d22395221418ae4796aa59
konstadinos-agoris <konstadinosaa@gmail.com>
2023-07-07 00:00:36 +0300

    aoi endpoint implementation

---
+++ backend/src/main/java/com/devdynasty/CrowdCritic/dto/AreaOfInterestDTO.java
@@ -0,0 +1,36 @@
+package com.devdynasty.CrowdCritic.dto;
+
+import com.devdynasty.CrowdCritic.model.AppUser;
+import com.devdynasty.CrowdCritic.model.AreaOfInterest;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+@Getter
+@Setter
+public class AreaOfInterestDTO {
+
+    private Integer id;
+
+    private Double latitude;
+    private Double longitude;
+
+    private Integer distance;
+
+
+    private List<UserDto> appUsers;
+
+
+    public AreaOfInterestDTO(AreaOfInterest areaOfInterest){
+
+        this.appUsers=areaOfInterest.getAppUsers()
+                .stream()
+                .map(UserDto::new)
+                .toList();
+    }
+}
